5cf2b732f3693b96c7810632f90f3d62b5ab4e6f
Anchal0410 <anchal's-email@example.com>
2025-05-02 11:42:08 +0530

    Fix accessibility issue: Add alt attribute to image

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,16 +1,16 @@
-import React from "react";
-import { FiFacebook, FiTwitter, FiInstagram } from "react-icons/fi";
+import React from 'react';
+import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
 
-import { FooterOverlay, Newsletter } from "../../components";
-import { images } from "../../constants";
-import "./Footer.css";
+import { FooterOverlay, Newsletter } from '../../components';
+import { images } from '../../constants';
+import './Footer.css';
 
 const Footer = () => (
   <div className="app__footer section__padding" id="login">
     <FooterOverlay />
     <Newsletter />
 
-     <div className="app__footer-links">
+    <div className="app__footer-links">
       <div className="app__footer-links_contact">
         <h1 className="app__footer-headtext">Contact Us</h1>
         <p className="p__opensans">11th Street, Andheri Mumbai</p>
@@ -21,14 +21,9 @@ const Footer = () => (
       <div className="app__footer-links_logo">
         <img src={images.gericht} alt="footer_logo" />
         <p className="p__opensans">
-          &quot;The best way to find yourself is to lose yourself in the service
-          of others.&quot;
+          &quot;The best way to find yourself is to lose yourself in the service of others.&quot;
         </p>
-        <img
-          src={images.spoon}
-          className="spoon__img"
-          style={{ marginTop: 15 }}
-        />
+        <img src={images.spoon} className="spoon__img" style={{ marginTop: 15 }} alt="spoon" />
         <div className="app__footer-links_icons">
           <FiFacebook />
           <FiTwitter />
